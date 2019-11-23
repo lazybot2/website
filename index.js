e0a655e9de8e1942a2505eed5021b9412fffb605
Asineth <asineth@pm.me>
2019-11-23 16:11:37 -0500

    New background

--- index.js
+++ index.js
@@ -1,54 +1,46 @@
 particlesJS("particles-js", {
     "particles": {
         "number": {
-            "value": 160,
+            "value": 80,
             "density": {
                 "enable": true,
                 "value_area": 800
             }
         },
         "color": {
-            "value": "#ffffff"
+            "value": ["#BD10E0", "#B8E986", "#50E3C2", "#FFD300", "#E86363"]
         },
         "shape": {
             "type": "circle",
             "stroke": {
                 "width": 0,
-                "color": "#000000"
-            },
-            "polygon": {
-                "nb_sides": 5
-            },
-            "image": {
-                "src": "img/github.svg",
-                "width": 100,
-                "height": 100
+                "color": "#b6b2b2"
             }
         },
         "opacity": {
-            "value": 1,
-            "random": true,
+            "value": 0.5211089197812949,
+            "random": false,
             "anim": {
                 "enable": true,
                 "speed": 1,
-                "opacity_min": 0,
+                "opacity_min": 0.1,
                 "sync": false
             }
         },
         "size": {
-            "value": 3,
+            "value": 8.017060304327615,
             "random": true,
             "anim": {
-                "enable": false,
-                "speed": 4,
-                "size_min": 0.3,
-                "sync": false
+                "enable": true,
+                "speed": 12.181158184520175,
+                "size_min": 0.1,
+                "sync": true
             }
         },
         "line_linked": {
-            "enable": false,
+            "enable": true,
             "distance": 150,
-            "color": "#ffffff",
+            "color": "#c8c8c8",
             "opacity": 0.4,
             "width": 1
         },
@@ -56,14 +48,14 @@ particlesJS("particles-js", {
             "enable": true,
             "speed": 1,
             "direction": "none",
-            "random": true,
+            "random": false,
             "straight": false,
-            "out_mode": "out",
+            "out_mode": "bounce",
             "bounce": false,
             "attract": {
                 "enable": false,
                 "rotateX": 600,
-                "rotateY": 600
+                "rotateY": 1200
             }
         }
     },
@@ -71,12 +63,12 @@ particlesJS("particles-js", {
         "detect_on": "canvas",
         "events": {
             "onhover": {
-                "enable": true,
-                "mode": "bubble"
+                "enable": false,
+                "mode": "repulse"
             },
             "onclick": {
-                "enable": true,
-                "mode": "repulse"
+                "enable": false,
+                "mode": "push"
             },
             "resize": true
         },
@@ -88,14 +80,14 @@ particlesJS("particles-js", {
                 }
             },
             "bubble": {
-                "distance": 250,
-                "size": 0,
+                "distance": 400,
+                "size": 40,
                 "duration": 2,
-                "opacity": 0,
+                "opacity": 8,
                 "speed": 3
             },
             "repulse": {
-                "distance": 400,
+                "distance": 200,
                 "duration": 0.4
             },
             "push": {
@@ -108,20 +100,3 @@ particlesJS("particles-js", {
     },
     "retina_detect": true
 });
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function () {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-};
-requestAnimationFrame(update);;
